61c20c826dc8815cab859e485ac3140e974bfc85
Wladimir Oliveira <wladimir4019@gmail.com>
2025-12-13 03:31:13 -0300

    add `shadcn`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <h1 className="">Hello, Next.js</h1>;
+import { GalleryVerticalEnd } from "lucide-react";
+
+import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary pr-[4px] pl-[2px] text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <Image
+              src="/logo-senna-block.svg"
+              alt="Logo HiperSenna"
+              width={38}
+              height={28}
+            />
+          </div>
+          GHS Sistema
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/login-form.tsx
@@ -0,0 +1,59 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Bem vindo</CardTitle>
+          <CardDescription>
+            Faça o login com seu nome de usuário e sua senha
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="username">Nome de Usuário</FieldLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="username"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
