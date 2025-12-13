9f00f7acd49c5cae1aa0cf31eeb6ad14e75f99bc
Wladimir Oliveira <wladimir4019@gmail.com>
2025-12-13 02:16:10 -0300

    configure `roboto` font to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={roboto.className}>
       <body>{children}</body>
     </html>
   );
